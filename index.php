6b8b460a8f22623f332f274d278fe84bb9933902
FatMrtnz <19893871@clases.edu.sv>
2026-05-20 08:06:12 -0600

    Este archivo es el principal
    Este archivo contiene los links de todos los ejercicios de la guía

---
+++ index.php
@@ -0,0 +1,52 @@
+   <h1> menu </h1> 
+            <br><br>
+            <a echo href="Ejercicio1.html"> Ejercicio1 </a>"
+            <br><br>
+             <a echo href="Ejercicio2.html"> Ejercicio2 </a>
+            <br><br>
+             <a echo href="Ejercicio3.html"> Ejercicio3  </a>
+            <br><br>
+             <a echo href="Ejercicio4.html"> Ejercicio4  </a>
+            <br><br>
+             <a echo href="Ejercicio5.html"> Ejercicio5  </a>
+            <br><br>
+             <a echo href="Ejercicio6.html"> Ejercicio6  </a>
+            <br><br>
+             <a  echo href="Ejercicio7.html"> Ejercicio7  </a>
+            <br><br>
+             <a echo href="Ejercicio8.html"> Ejercicio8  </a>
+            <br><br>
+             <a echo href="Ejercicio9.html"> Ejercicio9  </a>
+            <br><br>
+             <a echo href="Ejercicio10.html"> Ejercicio10  </a>
+            <br><br>
+             <a echo href="Ejercicio11.html"> Ejercicio11  </a>
+            <br><br>
+             <a echo href="Ejercicio12.html"> Ejercicio12  </a>
+            <br><br>
+             <a echo href="Ejercicio13.html"> Ejercicio13  </a>
+            <br><br>
+             <a echo href="Ejercicio14.html"> Ejercicio14  </a>
+            <br><br>
+             <a echo href="Ejercicio15.html"> Ejercicio15  </a>
+            <br><br>
+             <a echo href="Ejercicio16.html"> Ejercicio16  </a>
+            <br><br>
+             <a echo href="Ejercicio17.html"> Ejercicio17  </a>
+            <br><br>
+             <a echo href="Ejercicio18.html"> Ejercicio18  </a>
+            <br><br>
+             <a echo href="Ejercicio19.html"> Ejercicio19  </a>
+            <br><br>
+             <a echo href="Ejercicio20.html"> Ejercicio20  </a>
+            <br><br>
+             <a echo href="Ejercicio21.html"> Ejercicio21  </a>
+            <br><br>
+             <a echo href="Ejercicio22.html"> Ejercicio22  </a>
+            <br><br>
+             <a echo href="Ejercicio23.html"> Ejercicio23  </a>
+            <br><br>
+             <a echo href="Ejercicio24.html"> Ejercicio24  </a>
+            <br><br>
+             <a echo href="Ejercicio25.html"> Ejercicio25  </a>
+            <br><br>
